fcfd0351ed0594774b3f81161c080701a4bd162a
Dhaval Patel <dhavalmpatel2402@gmail.com>
2026-06-03 19:36:59 -0400

    Time: 252 ms (88.89%) | Memory: 21.9 MB (94.44%) - LeetSync

---
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def earliestFinishTime(self, lst, ldu, wst, wdu):
+        """
+        :type landStartTime: List[int]
+        :type landDuration: List[int]
+        :type waterStartTime: List[int]
+        :type waterDuration: List[int]
+        :rtype: int
+        """
+        ans = float("inf")
+        mln = float("inf")
+        mnw = float("inf")
+
+        for i in range(len(lst)):
+            mln = min(mln, lst[i] + ldu[i])
+
+        for i in range(len(wst)):
+            ans = min(ans, max(mln, wst[i]) + wdu[i])
+
+        for i in range(len(wst)):
+            mnw = min(mnw, wst[i] + wdu[i])
+
+        for i in range(len(lst)):
+            ans = min(ans, max(mnw, lst[i]) + ldu[i])
+
+        return ans
